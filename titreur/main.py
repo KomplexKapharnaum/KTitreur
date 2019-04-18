afd171a256f50caf54a2098f85634556f94ad685
maigre <thomas.bohl@gmail.com>
2019-04-18 22:40:58 +0200

    reorganise + catch udp send error

---
+++ titreur/main.py
@@ -0,0 +1,49 @@
+import signal, sys
+from hardware6 import Hardware6
+from textlist import Textlist
+from udpinterface import Udpinterface
+
+# RUN
+RUN = True
+
+def signal_handler(signal, frame):
+    global RUN
+    RUN = False
+signal.signal(signal.SIGINT, signal_handler)
+
+# HARDWARE
+hw = Hardware6('bin/hardware6')
+RUN = hw.start()
+
+# TEXTLIST
+texts = Textlist()
+texts.on('pick', hw.text)
+
+# RAW UDP
+UDP_PORT = 3742
+if len(sys.argv) >= 2:
+    UDP_PORT = int(sys.argv[1])
+udp = Udpinterface(UDP_PORT)
+udp.on('speed',     texts.autoPick )
+udp.on('scroll',    hw.scroll)
+udp.on('clear',     texts.clear)
+udp.on('add',       texts.add)
+udp.on('text',      texts.set)
+udp.on('tick',      texts.pick)
+
+# OSC
+
+
+# DEFAULT BEHAVIOUR
+texts.add( (" beaucoup / beaucoup", 'NO_SCROLL_NORMAL') )
+texts.add( ("beaucoup beaucoup", 'NO_SCROLL_BIG') )
+texts.add( ("           beaucoup /     beaucoup", 'SCROLL_LOOP_NORMAL') )
+texts.add( ("    beaucoup beaucoup", 'SCROLL_LOOP_BIG') )
+texts.autoPick(2000)
+
+# LOOP
+while RUN:
+    udp.check()
+
+# EXIT
+hw.stop()
---
+++ titreur/udpinterface.py
@@ -0,0 +1,61 @@
+from pyee import EventEmitter
+import socket
+
+class Udpinterface(EventEmitter):
+
+    # Init object
+    def __init__(self, port):
+        super().__init__()
+        self.sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+        self.sock.bind(("0.0.0.0", port))
+        self.sock.settimeout(0.1)
+        print("UDP listening on", port)
+
+    # Read UDP input
+    def check(self):
+        try:
+            data, address = self.sock.recvfrom(4096)
+            data = data.decode('utf-8').strip().split(' ')
+
+            # speed TIMEMIN [TIMEMAX]
+            if data[0] == "speed":
+            	if len(data) >= 3 and data[2] != "#":
+            		SPEEDMIN = int(data[1])
+            		SPEEDMAX = int(data[2])
+            	elif len(data) >= 2:
+            		SPEEDMIN = int(data[1])
+            		SPEEDMAX = int(data[1])
+            	self.emit('speed', (SPEEDMIN, SPEEDMAX))
+
+            # scrool speed
+            elif data[0] == "scroll":
+            	if len(data) >= 2:
+            		SCROLLSPEED = int(data[1])
+            		self.emit('scroll', SCROLLSPEED)
+
+            # clear
+            elif data[0] == "clear":
+            	self.emit('clear')
+
+            # add text
+            elif data[0] == "add":
+            	if len(data) >= 3:
+            		mode = data[1]
+            		txt = "_".join(data[2:])
+            		self.emit('add', (txt, mode))
+
+            # text
+            elif data[0] == "text":
+            	if len(data) >= 3:
+            		mode = data[1]
+            		txt = "_".join(data[2:])
+            		self.emit('text', (txt, mode))
+
+            # tick
+            elif data[0] == "tick":
+            	self.emit('tick')
+
+
+        except socket.timeout:
+            pass
