ad8bab673e9739dfc4593e79cbb29275a3293b31
maigre <thomas.bohl@gmail.com>
2019-07-04 16:50:34 +0200

    read id

--- titreur/mqttinterface.py
+++ titreur/mqttinterface.py
@@ -16,7 +16,7 @@ def on_subscribe(client, userdata, mid, granted_qos):
 
 def on_message(client, userdata, message):
     print("MQTT: Received message '" + str(message.payload) + "' on topic '" + message.topic + "' with QoS " + str(message.qos))
-    command  = message.topic.split('/')[1:]
+    command  = message.topic.split('/')[2:]
     args = message.payload.decode().split('§')
     userdata.emit(command[0], tuple(args))
     print("--", command[0], tuple(args))
@@ -46,52 +46,4 @@ class Mqttinterface(EventEmitter):
     def stop(self):
         self.client.loop_stop(True)
 
-
-    # Read UDP input
-    def callback(self):
-        try:
-            data, address = self.sock.recvfrom(4096)
-            data = data.decode('utf-8').strip().split(' ')
-
-            # speed TIMEMIN [TIMEMAX]
-            if data[0] == "speed":
-            	if len(data) >= 3 and data[2] != "#":
-            		SPEEDMIN = int(data[1])
-            		SPEEDMAX = int(data[2])
-            	elif len(data) >= 2:
-            		SPEEDMIN = int(data[1])
-            		SPEEDMAX = int(data[1])
-            	self.emit('speed', (SPEEDMIN, SPEEDMAX))
-
-            # scrool speed
-            elif data[0] == "scroll":
-            	if len(data) >= 2:
-            		SCROLLSPEED = int(data[1])
-            		self.emit('scroll', SCROLLSPEED)
-
-            # clear
-            elif data[0] == "clear":
-            	self.emit('clear')
-
-            # add text
-            elif data[0] == "add":
-            	if len(data) >= 3:
-            		mode = data[1]
-            		txt = "_".join(data[2:])
-            		self.emit('add', (txt, mode))
-
-            # text
-            elif data[0] == "text":
-            	if len(data) >= 3:
-            		mode = data[1]
-            		txt = "_".join(data[2:])
-            		self.emit('text', (txt, mode))
-
-            # tick
-            elif data[0] == "tick":
-            	self.emit('tick')
-
-
-        except socket.timeout:
-            pass
 
